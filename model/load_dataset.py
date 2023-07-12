f46895c7e6f2947d3d619cea51ee7fca30aeacf7
atom <atom1905h@naver.com>
2023-07-12 00:00:47 +0000

    [refactor] class roboflow dataset

--- model/load_dataset.py
+++ model/load_dataset.py
@@ -1,9 +1,8 @@
 from roboflow import Roboflow
 class roboflow_dataset:
-    def __init__(self, version):
+    def __init__(self, version, project):
         self.version = version
+        self.project = project
     def load_dataset(self):
-        rf = Roboflow(api_key="I56H1gcAvLeqblSIcWF9")
-        project = rf.workspace("final-projectboostcamp-ai-tech").project("final-project-dataset-r1wxc")
-        dataset = project.version(self.version).download("yolov8")
+        dataset = self.project.version(self.version).download("yolov8")
         return dataset
