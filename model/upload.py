9cb9ee8620abaf51c45bec13491d4733cf7305c0
atom <atom1905h@naver.com>
2023-07-13 04:14:38 +0000

    [feat] add extension

--- model/upload.py
+++ model/upload.py
@@ -4,13 +4,14 @@ import yaml
 import random
 
 def upload_data(config, project):
-    image_extension_type = '.jpg'
+    image_extension_type = ['.jpg','.png']
     label_extension_type = '.txt'
 
     new_image_path = config['new_image_path']
     new_label_path = config['new_label_path']
-
-    image_glob = glob.glob(new_image_path + '/*' + image_extension_type)
+    image_glob=[]
+    for extension_type in image_extension_type:
+        image_glob.extend(glob.glob(new_image_path + '/*' + extension_type))
     label_glob = glob.glob(new_label_path+'/*'+ label_extension_type)
 
     new_data = list(zip(image_glob, label_glob))
