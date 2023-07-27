c051a29d322c7a32a870279c54f5602b5098d4c6
atom <atom1905h@naver.com>
2023-07-27 04:36:26 +0000

    [feat] add save model version

--- model/train.py
+++ model/train.py
@@ -18,8 +18,11 @@ def save_config(config, file_path):
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--epochs",type=int, default=3000)
-    parser.add_argument("--patience", type=int, default=100)
-    parser.add_argument("--update",type=bool, default=False)
+    parser.add_argument("--patience", type=int, default=150)
+    parser.add_argument("--update",type=str, default="False")
+    parser.add_argument("--opt",type=str, default='auto')
+    parser.add_argument("--batch",type=int, default=16)
+    parser.add_argument("--mosaic",type=float, default=1.0)
     args = parser.parse_args()
     return args
 
@@ -29,6 +32,7 @@ config = load_config(config_path)
 secret = load_config(secret_path)
 
 current_version = config.get('dataset_version')
+current_model_version = config.get('model_version')
 args = parse_args()
 
 # project 불러오기
@@ -36,17 +40,19 @@ rf = Roboflow(api_key=secret['api_key'])
 project = rf.workspace(config['workspace']).project(config['project'])
 
 
-if args.update:
+if args.update=="True":
     current_version += 1
+    current_model_version += 1
     config['dataset_version'] = current_version
+    config['model_version'] = current_model_version
     upload.upload_data(config, project)
     generate_dataset.generate_dataset(project)
-    save_config(config, config_path)
 
 # dataset
 dataset = roboflow_dataset(config['dataset_version'], project)
 dataset = dataset.load_dataset()
-
+config['data_dir']=dataset.location+'/data.yaml'
+save_config(config, config_path)
 #train
 model = YOLO(config['model'])
-results= model.train(data=dataset.location+'/data.yaml',epochs=args.epochs,patience=args.patience)
+results= model.train(data=dataset.location+'/data.yaml',epochs=args.epochs,patience=args.patience, optimizer=args.opt, batch = args.batch,mosaic=args.mosaic)
